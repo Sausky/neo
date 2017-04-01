6c80fa3e044339468ac86ee971c99efa425bef93
Sausky <735507752@qq.com>
2017-04-01 15:41:31 +0800

    Letv
    add support

--- test.py
+++ test.py
@@ -2,11 +2,13 @@
 import sys
 import os
 import sites.mgtv
+import sites.letv
 
 """write your code for test
 """
 if __name__ == "__main__":
     mgtv_test = sites.mgtv.MGTV()
     print(mgtv_test.for_test(orig_url='http://www.hunantv.com/v/2/309238/f/3761306.html'))
+
 
 
